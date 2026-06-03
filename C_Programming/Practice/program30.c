b299923951a697bac9ed2de5725335d98e873f04
vaishnav-shingote <vaishnavshingote2006@gmail.com>
2026-06-03 23:16:59 +0530

    Programming based on for loop

---
+++ C_Programming/Practice/program30.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void Display(int iNo)
+{
+    // Filter
+    if (iNo < 0)
+    {
+        printf("Invalid input\n");
+        return;
+    }
+    int iCnt = 0;
+
+    for (iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("Jay Ganesh...\n");
+    }
+}
+
+int main()
+{
+
+    int iValue = 0;
+
+    printf("Enter the frequency : ");
+    scanf("%d", &iValue);
+
+    Display(iValue);
+
+    return 0;
+}
