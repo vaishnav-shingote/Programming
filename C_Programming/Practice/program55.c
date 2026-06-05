b0f20d826d4793693650643b781cb0301316c67c
vaishnav-shingote <vaishnavshingote2006@gmail.com>
2026-06-05 14:56:46 +0530

    Programming based on numbers using for loop'

---
+++ C_Programming/Practice/program55.c
@@ -0,0 +1,28 @@
+// Input : 8
+// Output : 1 2 3 4 5 6 7
+
+#include <stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    for (iCnt = 1; iCnt < iNo; iCnt = iCnt + 1)
+    {
+        printf("%d\t", iCnt);
+    }
+    printf("\n");
+}
+
+int main()
+{
+
+    int iValue = 0;
+
+    printf("Enter number : ");
+    scanf("%d", &iValue);
+
+    Display(iValue);
+
+    return 0;
+}
