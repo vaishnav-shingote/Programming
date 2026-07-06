3e7eaa02d50c49820f9bb50e375d6fa8e2e951de
vaishnav-shingote <vaishnavshingote2006@gmail.com>
2026-07-06 21:01:46 +0530

    Pattern Printing

---
+++ C_Programming/Competative/Question130.c
@@ -0,0 +1,44 @@
+// Accept number of rows and number of columns from user and display below pattern.
+/*
+Input : iRow = 6    iCol = 6
+Output :
+        1   2   3   4
+            2   3   4
+                3   4
+                    5
+*/
+
+#include <stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0, iCnt = 0;
+
+    for (i = 1; i <= iRow; i++)
+    {
+        for (j = 1; j <= iCol; j++)
+        {
+            if (j < i)
+            {
+                printf("\t");
+            }
+            else
+            {
+                printf("%d\t", j);
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and columns : \n");
+    scanf("%d %d", &iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
