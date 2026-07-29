a821963f6c52a59d91853bffe194de34b078bafc
sejals2 <shingotesejal2004@gmail.com>
2026-07-29 18:51:10 +0530

    Programming on Implementation of Doubly LL with basic oprations.

---
+++ C_Programming/DS/program438.c
@@ -0,0 +1,171 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+int Count(PNODE first, PNODE last)
+{
+    int iCount = 0;
+    if (first == NULL || last == NULL)
+    {
+        return 0;
+    }
+
+    printf(" <=> ");
+    do
+    {
+        iCount++;
+        first = first->next;
+    } while (first != last->next);
+
+    return iCount;
+}
+
+void Display(PNODE first, PNODE last)
+{
+    if (first == NULL || last == NULL)
+    {
+        return;
+    }
+
+    printf(" <=> ");
+    do
+    {
+        printf("| %d | <=> ", first->data);
+        first = first->next;
+    } while (first != last->next);
+    printf("\n");
+}
+
+void InsertFirst(PPNODE first, PPNODE last, int iNo)
+{
+    PNODE newn = (PNODE)malloc(sizeof(NODE));
+    newn->data = iNo;
+    newn->next = NULL;
+    newn->prev = NULL;
+
+    if (*first == NULL && *last == NULL)
+    {
+        *first = newn;
+        *last = newn;
+    }
+    else
+    {
+        (*last)->next = newn;
+        newn->prev = *last;
+        *last = newn;
+    }
+    (*last)->next = *first;
+    (*first)->prev = *last;
+}
+void InsertLast(PPNODE first, PPNODE last, int iNo)
+{
+    PNODE newn = (PNODE)malloc(sizeof(NODE));
+    newn->data = iNo;
+    newn->next = NULL;
+    newn->prev = NULL;
+
+    if (*first == NULL && *last == NULL)
+    {
+        *first = newn;
+        *last = newn;
+    }
+    else
+    {
+        (*last)->next = newn;
+        newn->prev = *last;
+    }
+    (*last)->next = *first;
+    (*first)->prev = *last;
+}
+void InsertAtPos(PPNODE first, PPNODE last, int iNo, int iPos)
+{
+}
+
+void DeleteFirst(PPNODE first, PPNODE last)
+{
+    if (*first == NULL && *last == NULL)
+    {
+        printf("No node to delete\n");
+        return;
+    }
+    else if (*first == *last)
+    {
+        free(*first);
+        *first = NULL;
+        *last = NULL;
+    }
+    else
+    {
+        *first = (*first)->next;
+        free((*first)->prev);
+        (*last)->next = *first;
+        (*first)->prev = *last;
+    }
+}
+void DeleteLast(PPNODE first, PPNODE last)
+{
+    if (*first == NULL && *last == NULL)
+    {
+        printf("No node to delete\n");
+        return;
+    }
+    else if (*first == *last)
+    {
+        free(*first);
+        *first = NULL;
+        *last = NULL;
+    }
+    else
+    {
+        *last = (*last)->prev;
+        free((*last)->next);
+
+        (*last)->next = *first;
+        (*first)->prev = *last;
+    }
+}
+void DeleteAtPos(PPNODE first, PPNODE last, int iPos)
+{
+}
+
+int main()
+{
+    int iRet = 0;
+    PNODE head = NULL;
+    PNODE tail = NULL;
+
+    InsertFirst(&head, &tail, 51);
+    InsertFirst(&head, &tail, 21);
+    InsertFirst(&head, &tail, 11);
+
+    InsertLast(&head, &tail, 101);
+    InsertLast(&head, &tail, 111);
+    InsertLast(&head, &tail, 121);
+
+    Display(head, tail);
+    iRet = Count(head, tail);
+    printf("Number of nodes are : %d\n", iRet);
+
+    DeleteFirst(&head, &tail);
+    Display(head, tail);
+    iRet = Count(head, tail);
+    printf("Number of nodes are : %d\n", iRet);
+
+    DeleteLast(&head, &tail);
+    Display(head, tail);
+    iRet = Count(head, tail);
+    printf("Number of nodes are : %d\n", iRet);
+
+    return 0;
+}
