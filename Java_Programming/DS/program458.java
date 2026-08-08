50e6331848fbc0ae65e6b9d6b21f304d7f212662
sejals2 <shingotesejal2004@gmail.com>
2026-08-08 18:30:30 +0530

    Doubly LL with all basic operations

---
+++ Java_Programming/DS/program458.java
@@ -0,0 +1,252 @@
+class node
+{
+    public node prev;
+    public int data;
+    public node next;
+
+    node(int no)
+    {
+        this.prev = null;
+        this.data = no;
+        this.next = null;
+    }
+}
+
+class DoublyLL
+{
+    private node first;
+    private int iCount;
+
+    public DoublyLL()
+    {
+        this.first = null;
+        this.iCount = 0;
+    }
+
+    public void Display()
+    {
+        node temp = first;
+
+        System.out.println("null <- ");
+
+        while(temp != null)
+        {
+            System.out.print("| "+temp.data+" | -> ");
+            temp = temp.next;
+        }
+
+        System.out.println("null");
+    }
+
+    public int Count()
+    {
+        return iCount;
+    }
+
+    public void InsertFirst(int iNo)
+    {
+        node newn = new node(iNo);
+
+        if(first == null)
+        {
+            first = newn;
+        }
+        else
+        {
+            newn.next = first;
+            first.prev = newn;
+            first = newn;
+        }
+        iCount++;
+    }
+
+    public void InsertLast(int iNo)
+    {
+        node newn = new node(iNo);
+
+        if(first == null)
+        {
+            first = newn;
+        }
+        else
+        {
+            node temp = first;
+
+            while(temp.next != null)
+            {
+                temp = temp.next;
+            }
+
+            temp.next = newn;
+            newn.prev = temp;
+        }
+        iCount++;        
+    }
+
+    public void InsertAtPos(int iNo, int iPos)
+    {
+        node temp = null;
+        node newn = null;
+        int i = 0;
+        
+        if((iPos < 1) || (iPos > iCount+1))
+        {
+            return;
+        }
+
+        if(iPos == 1)
+        {
+            InsertFirst(iNo);
+        }
+        else if(iPos == iCount+1)
+        {
+            InsertLast(iNo);
+        }
+        else
+        {
+            newn = new node(iNo);
+
+            temp = first;
+
+            for(i = 1; i < iPos-1; i++)
+            {
+                temp = temp.next;
+            }
+
+            newn.next = temp.next;
+            newn.prev = temp;
+            temp.next.prev = newn;
+            temp.next = newn;
+
+            iCount++;
+        }
+    }
+
+    public void DeleteFirst()
+    {
+        if(first == null)
+        {
+            return;
+        }
+        else if(first.next == null)
+        {
+            first = null;
+        }
+        else
+        {
+            first = first.next;
+            first.prev = null;
+        }
+        iCount--;
+    }
+    
+    public void DeleteLast()
+    {
+        if(first == null)
+        {
+            return;
+        }
+        else if(first.next == null)
+        {
+            first = null;
+        }
+        else
+        {
+            node temp = first;
+
+            while(temp.next.next != null)
+            {
+                temp = temp.next;
+            }
+
+            temp.next.prev = null;
+            temp.next = null;
+        }
+        iCount--;        
+    }
+
+    public void DeleteAtPos(int iPos)
+    {
+        node temp = null;
+        int i = 0;
+        
+        if((iPos < 1) || (iPos > iCount))
+        {
+            return;
+        }
+
+        if(iPos == 1)
+        {
+            DeleteFirst();
+        }
+        else if(iPos == iCount)
+        {
+            DeleteLast();
+        }
+        else
+        {
+            temp = first;
+
+            for(i = 1; i < iPos-1; i++)
+            {
+                temp = temp.next;
+            }
+
+            temp.next = temp.next.next;
+            temp.next.prev = temp;
+
+            iCount--;
+                
+        }
+    }
+}
+
+class program458
+{
+    public static void main(String A[])
+    {
+        int iRet = 0;
+
+        DoublyLL sobj = new DoublyLL();
+        
+        sobj.InsertFirst(51);
+        sobj.InsertFirst(21);
+        sobj.InsertFirst(11);
+
+        sobj.InsertLast(101);
+        sobj.InsertLast(111);
+        sobj.InsertLast(121);
+        
+        sobj.Display();
+
+        iRet = sobj.Count();
+
+        System.out.println("Number of nodes are : "+iRet);
+
+        sobj.DeleteFirst();
+        sobj.DeleteLast();
+
+        sobj.Display();
+
+        iRet = sobj.Count();
+
+        System.out.println("Number of nodes are : "+iRet);
+
+        sobj.InsertAtPos(105,4);
+
+        sobj.Display();
+
+        iRet = sobj.Count();
+
+        System.out.println("Number of nodes are : "+iRet);
+
+        sobj.DeleteAtPos(4);
+
+        sobj.Display();
+
+        iRet = sobj.Count();
+
+        System.out.println("Number of nodes are : "+iRet);
+    }
+}
+
